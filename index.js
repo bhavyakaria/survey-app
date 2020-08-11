1fd10f5944809251edc9339fe6574230a28b7e36
bhavyankaria <bhavya.karia@go-upwards.com>
2020-08-11 21:57:37 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app = express();
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+const app = express();
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
